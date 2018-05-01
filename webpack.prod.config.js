6d6b8bdf2224b3f7676a30b57015d199de157015
C5MKID <596584742@qq.com>
2018-05-02 00:16:12 +0800

    解决build出错的问题

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,38 +1,41 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const merge = require('webpack-merge');
-const webpackBaseConfig = require('./webpack.config.js');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var merge = require('webpack-merge');
+var webpackBaseConfig = require('./webpack.config.js');
 
+// 清空基本配置的插件列表
 webpackBaseConfig.plugins = [];
 
 module.exports = merge(webpackBaseConfig, {
-  output: {
-    publicPath: '/dist/',
-    filename: '[name].[hash:5].js'
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: '[name].[hash:5].css',
-      allChunks: true
-    }),
-
-    new webpack.DefinePlugin({
-      'process_env': {
-        'NODE_ENV': '"production"'
-      }
-    }),
-
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: '../index_prod.html',
-      template: './index.ejs',
-      inject: false
-    })
-  ]
+    output: {
+        publicPath: '/dist/',
+        // 将入口文件重命名为带有 5 位 hash 值的唯一文件
+        filename: '[name].[hash:5].js'
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            // 提取 css，并重命名为带有 5 位 hash 值的唯一文件
+            filename: '[name].[hash:5].css',
+            allChunks: true
+        }),
+        // 定义当前 node 环境为生产环境
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        // 压缩 js
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        // 提取模板，并保存入口 html 文件
+        new HtmlWebpackPlugin({
+            filename: '../index_prod.html',
+            template: './index.ejs',
+            inject: false
+        })
+    ]
 });
